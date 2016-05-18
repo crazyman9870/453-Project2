21fa69045a8cd8e11dcee6076f0a62f0f10a2242
crazyman9870 <crazyman9870@gmail.com>
2016-05-18 10:51:49 -0600

    Tokenizing the logs

--- src/webQS/TrieLoader.java
+++ src/webQS/TrieLoader.java
@@ -1,23 +1,34 @@
 package webQS;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class TrieLoader {
-	
-	private Scanner scan;
-	
+		
 	public TrieLoader() {
-		scan = null;
 	}
 	
 	public void loadTrie(Trie trie) {
 		File path = new File("logs");
 		for(File file : path.listFiles()) {
 			try {
-				scan = new Scanner(file);
-			} catch (FileNotFoundException e) {
+				BufferedReader buffReader = new BufferedReader(new FileReader(file));
+				StringBuffer strBuff = new StringBuffer(); //Used to build the query that will be stored in the trie
+				String nextLine = "";
+				while ((nextLine = buffReader.readLine()) != null) {
+					StringTokenizer tokens = new StringTokenizer(nextLine);
+					System.out.println(tokens.countTokens());
+				}
+				buffReader.close();
+				//save it to a bin tree.
+				
+				
+			} catch (IOException e) {
 				System.out.println("Error thrown when loading the trie");
 				e.printStackTrace();
 			}
--- src/webQS/WebQS.java
+++ src/webQS/WebQS.java
@@ -5,6 +5,7 @@ public class WebQS {
 	public static void main(String[] args) {
 		Trie trie = new Trie();
 		TrieLoader loader = new TrieLoader();
+		loader.loadTrie(trie);
 		
 		trie.add("THIS WORD");
 		trie.add("something else");
